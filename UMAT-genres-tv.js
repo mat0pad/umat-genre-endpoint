e080674d0dfe78ea9359287aa12da8016949572a
mat0pad <mat_hias123@hotmail.com>
2017-01-14 16:40:45 +0100

    initial

---
+++ UMAT-genres-tv.js
@@ -0,0 +1,98 @@
+{
+    "genres": [
+               
+               {
+               
+               "url": "https://image.tmdb.org/t/p/w500/A30ZqEoDbchvE7mCZcSp6TEwB1Q.jpg",
+               "title":"Action & Adventure",
+               "id":"10759"
+			   },
+			   
+               {
+               "url": "https://image.tmdb.org/t/p/w500/6En6rMb8vGFWhkoDAv6paUioCIW.jpg",
+               "title":"Animation",
+               "id":"16"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/ndhrAoDsKSGlrAHEV5bDZ3JOqYI.jpg",
+               "title":"Comedy",
+               "id":"35"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/gmVSYAD69bBuayMA0Kvnic2tAcm.jpg",
+               "title":"Documentary",
+               "id":"99"
+			   },
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/vwHUdM5NmDw6xtE1pNpAiwb0TF3.jpg",
+               "title":"Drama",
+               "id":"18"
+			  },
+              
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/f5uNbUC76oowt5mt5J9QlqrIYQ6.jpg",
+               "title":"Family",
+               "id":"10751"
+			  },
+              
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/YZ0JbErYXPomaddgiSOLXHi4QG.jpg",
+               "title":"Kids",
+               "id":"10762"
+			  },
+			   
+			  { 
+               "url": "https://image.tmdb.org/t/p/w500/hD76ZDNjCAbGrKzAYJAHVCJ2ba7.jpg",
+               "title":"Mystery",
+               "id":"9648"
+	          },
+              
+			  {			  
+               "url": "https://image.tmdb.org/t/p/w500/3qMV0Wewf3j8lmZeRvwe4EIVPpn.jpg",
+               "title":"News",
+               "id":"10763"
+              }, 
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/al70ijZyPnMH1C4tQKMZHPvpoA9.jpg",
+               "title":"Reality",
+               "id":"10764"
+			  },
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg",
+               "title":"Sci-Fi & Fantasy ",
+               "id":"10765"
+			  },
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/icDbta6aL4tfz9xd4ThFQ6wVzDQ.jpg",
+               "title":"Soap",
+               "id":"10766"
+			  },
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/mMEirWNGIhjqx2mPU33PIyjC6wQ.jpg",
+               "title":"Talk",
+               "id":"10767"
+			  },
+			  
+			  {
+               "url": "https://image.tmdb.org/t/p/w500/jzGmCse4n1i1J6keW8cA5NyytSP.jpg",
+               "title":"War & Politics",
+               "id":"10768"
+			  },
+               
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/cZlnfCmfw3QESnmPHD62Q0ERhEg.jpg",
+               "title":"Western",
+               "id":"37"
+			   }
+               
+           
+               ]
+    
+}
---
+++ UMAT-genres.js
@@ -0,0 +1,126 @@
+{
+    "genres": [
+               
+               {
+               
+               "url": "https://image.tmdb.org/t/p/w500/wSyHVm2rv9jHxBqjAfSXJGSAYJ9.jpg",
+			   "title":"Action",
+               "id":"28"
+			   },
+               
+               {
+               "url": "https://image.tmdb.org/t/p/w500/mnUbzbYxaaHpaTqFhjl7HwSod7o.jpg",
+               "title":"Adventure",
+               "id":"12"
+               },
+              
+               {
+                "url": "https://image.tmdb.org/t/p/w500/3HuUMZOTmFwMsoH3aCbVKgTGjXZ.jpg",
+                "title":"Animation",
+                "id":"16"
+               },
+               
+			   { 
+               "url": "https://image.tmdb.org/t/p/w500/zchSMYxvRivumqWhnsYZFyhriTy.jpg",
+               "title":"Comedy",
+               "id":"35"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/mmBmAmpjEPGbx7KFDavXFRtA7a3.jpg",
+               "title":"Crime",
+               "id":"80"
+			   },
+               
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/vqZ2Eo7Xo4HyKLCoIOOSbUastic.jpg",
+               "title":"Documentary",
+               "id":"99"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/a6VvlnIRTJTPUnyf1tIV9kEJO27.jpg",
+               "title":"Drama",
+               "id":"18"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/2azgMtH7vt5av3OgqdiMJfjn3EH.jpg",
+               "title":"Family",
+               "id":"10751"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/eIOTsGg9FCVrBc4r2nXaV61JF4F.jpg",
+               "title":"Fantasy",
+               "id":"14"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/7mV7fLNcXMKJY7KPwfshvwdmUXn.jpg",
+               "title":"Foreign",
+               "id":"10769"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/qcb6z1HpokTOKdjqDTsnjJk0Xvg.jpg",
+               "title":"History",
+               "id":"36"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/i5oEwnSJpbi8L1HOCP3aibkExHU.jpg",
+               "title":"Horror",
+               "id":"27"
+			   },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/qU8IpZlBNsTHNcApMHjtoQb4wp2.jpg",
+               "title":"Music",
+               "id":"10402"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/aOvgn3E5OjJDepApd1WtS2doqMF.jpg",
+               "title":"Mystery",
+               "id":"9648"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/czjU8NpPVTx5NzEk0zxe433xN9R.jpg",
+               "title":"Romance",
+               "id":"10749"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/t5KONotASgVKq4N19RyhIthWOPG.jpg",
+               "title":"Science Fiction",
+               "id":"878"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/i9FoSPt3wSghZIZWpJeAmzc95Z6.jpg",
+               "title":"TV Movie",
+               "id":"10770"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/zSoiLv4sXbKlcbmx32TgnjENkg0.jpg",
+               "title":"Thriller",
+               "id":"53"
+               },
+			   
+			   {
+               "url": "https://image.tmdb.org/t/p/w500/pKawqrtCBMmxarft7o1LbEynys7.jpg",
+               "title":"War",
+               "id":"10752"
+			   },
+			   
+              {
+               "url": "https://image.tmdb.org/t/p/w500/sSvgNBeBNzAuKl8U8sP50ETJPgx.jpg",
+               "title":"Western",
+               "id":"37"
+               }
+               
+               ]
+}
